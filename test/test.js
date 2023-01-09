dcc80e73f02d2a7748019b047ec7513a10558e73
JP Lomas <jp@theqrl.org>
2023-01-09 16:50:57 +0000

    Revert CI sudo and refactor tests

--- test/test.js
+++ test/test.js
@@ -8,13 +8,6 @@ var assert = chai.assert
 var ip = 'mainnet-3.automated.theqrl.org'
 var port = '19009'
 
-var state = 0
-
-// process.on('unhandledRejection', error => {
-  // uncomment to review unhandled Promise rejections to debug
-  // console.log('unhandledRejection', error.message)
-// })
-
 describe('#mainnet', function() {
   var mainnet = new QrlNode(ip, port)
   it(`.version should report same version as in npm package.json file (=${process.env.npm_package_version})`, function() {
@@ -40,21 +33,12 @@ describe('#mainnet', function() {
           if (error) {
             throw new Error(error)
           }
-          state = 1
           resolve(response.node_info.state)
         })
       })
     }
     await expect(node()).to.eventually.equal('SYNCED')
   })
-
-  var check = function(done) {
-    if (state === 1) {
-      done()
-    } else {
-      setTimeout( function() { check(done) }, 1000)
-    }
-  }
 
   it('expect GetOTS to function if called from existing client connection', async function() {
     async function node() {
@@ -67,7 +51,6 @@ describe('#mainnet', function() {
           if (error) {
             throw new Error(error)
           }
-          state = 2
           resolve(response.unused_ots_index_found)
         })
       })
@@ -75,29 +58,12 @@ describe('#mainnet', function() {
     await expect(node()).to.eventually.equal(true)
   })
 
-  var check = function(done) {
-    if (state === 2) {
-      done()
-    } else {
-      setTimeout( function() { check(done) }, 1000)
-    }
-  }
-
   it('a second attempted connection should have its Promise rejected', async function() {
     async function node() {
       return await mainnet.connect()
     }
     await expect(node()).to.eventually.be.rejected
-    state = 3
   })
-  
-  var check = function(done) {
-    if (state === 3) {
-      done()
-    } else {
-      setTimeout( function() { check(done) }, 1000)
-    }
-  }
 
   it('an invalid node ip/port should result in a null connecion', async function() {
     async function node() {
@@ -110,21 +76,10 @@ describe('#mainnet', function() {
       } catch (error) {
         // consolse.log('er:', error)
       }
-      state = 4
       return testnet.connection
     }
     await expect(node()).to.eventually.equal(false)
   })
-
-  var check = function (done) {
-    if (state === 4) {
-      done()
-    } else {
-      setTimeout(function () {
-        check(done)
-      }, 1000)
-    }
-  }
 
   it('testnet node should have \'Final Testnet\' as its network_id', async function() {
     async function node() {
@@ -138,7 +93,6 @@ describe('#mainnet', function() {
           if (error) {
             throw new Error(error)
           }
-          state = 5
           resolve(response.node_info.network_id)
         })
       })
@@ -146,84 +100,30 @@ describe('#mainnet', function() {
     await expect(node()).to.eventually.equal('Final Testnet')
   })
 
-  var check = function (done) {
-    if (state === 5) {
-      done()
-    } else {
-      setTimeout(function () {
-        check(done)
-      }, 1000)
-    }
-  }
-
   it('.disconnect() should reset client and return .connection = false', function() {
     mainnet.disconnect()
     var status = mainnet.connection
     var client = mainnet.client
     expect(status).to.equal(false)
     expect(client).to.equal(null)
-    state = 6
   }) 
-
-  var check = function (done) {
-    if (state === 6) {
-      done()
-    } else {
-      setTimeout(function () {
-        check(done)
-      }, 1000)
-    }
-  }
 
   it('reconnection connection should have its Promise resolve', async function() {
     async function node() {
       return await mainnet.connect()
     }
     await expect(node()).to.eventually.not.be.rejected
-    state = 7
   })
-
-  var check = function (done) {
-    if (state === 7) {
-      done()
-    } else {
-      setTimeout(function () {
-        check(done)
-      }, 1000)
-    }
-  }
 
   it('expect GetOTS to be reported as a valid API call', async function() {
     await expect(mainnet.validApi('GetOTS')).to.eventually.not.be.rejected
     await expect(mainnet.validApi('GetOTS')).to.eventually.to.equal(true)
-    state = 8
-  })  // await node to be ready before checking if API state is checkable
-
-  var check = function (done) {
-    if (state === 8) {
-      done()
-    } else {
-      setTimeout(function () {
-        check(done)
-      }, 1000)
-    }
-  }
+  })
 
   it('expect ThisIsInvalid to be reported as an invalid API call', async function() {
     await expect(mainnet.validApi('ThisIsInvalid')).to.eventually.not.be.rejected
     await expect(mainnet.validApi('ThisIsInvalid')).to.eventually.to.equal(false)
-    state = 9
   })
-
-  var check = function (done) {
-    if (state === 9) {
-      done()
-    } else {
-      setTimeout(function () {
-        check(done)
-      }, 1000)
-    }
-  }
 
   it('mainnet node should report SYNCED after a GetStats API call', async function() {
     await expect(mainnet.api('GetStats')).to.eventually.have.property('node_info').property('state').equal('SYNCED')
